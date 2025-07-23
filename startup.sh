86e25e953adb29c4dec0a6471f81c269cd3d93bd
Julian Geiger <julian.geiger@psi.ch>
2025-07-23 13:59:36 +0200

    Re-add startup.sh

---
+++ startup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+
+bash $SCRIPT_DIR/post-init.sh
+
+jupyter server --ServerApp.ip=0.0.0.0 --ServerApp.port=8888 --ServerApp.base_url=$RENKU_BASE_URL_PATH --ServerApp.token="" --ServerApp.password="" --ServerApp.allow_remote_access=true --ContentsManager.allow_hidden=true --ServerApp.allow_origin=* --ServerApp.root_dir="/home/jovyan/work"
+
